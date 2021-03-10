534ecdfffef86a7d2e312cf84feaab869ca43221
pete <tchirkov.petter@gmail.com>
2021-03-11 00:18:58 +0200

    m

--- src/app.js
+++ src/app.js
@@ -24,31 +24,56 @@ import 'swiper/swiper-bundle.css';
  // configure Swiper to use modules
  SwiperCore.use([Navigation, Pagination]);
 
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters
-  loop: true,
-  fadeEffect: { crossFade: true },
-  autoplay: {
-     delay: 2500,
-     disableOnInteraction: true,
-  },
-  effect: "fade",
-
-  // If we need pagination
+ var swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
-    type: 'fraction'
+    type: 'fraction',
   },
-
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
 });
-
+$(document).ready(function(){
+  // $('.swiper-wrapper').slick({
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   dots:true,
+  //   centerMode: true,
+  //   responsive: [{
+  //     breakpoint: 1024,
+  //     settings: {
+  //       slidesToShow: 3,
+  //       slidesToScroll: 1,
+  //       // centerMode: true,
+  
+  //     }
+  
+  //   }, {
+  //     breakpoint: 800,
+  //     settings: {
+  //       slidesToShow: 2,
+  //       slidesToScroll: 2,
+  //       dots: true,
+  //       infinite: true,
+  
+  //     }
+  //   },  {
+  //     breakpoint: 480,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1,
+  //       dots: true,
+  //       infinite: true,
+  //       autoplay: true,
+  //       autoplaySpeed: 2000,
+  //     }
+  //   }]
+  // });
+})
 
 
 function getVals(){
